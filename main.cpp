aa2a7e5059cb77b2d10db144f7c943371058870e
Anang <tyoandini@gmail.com>
2021-05-29 18:08:20 +0700

    menerapkan backspace di key

--- main.cpp
+++ main.cpp
@@ -3,21 +3,30 @@
 
 int main(){
 	char c;
-	string input = "Input Pencarian : ";
 	int p_input;
+	string key = "";
+	string input = "Input Pencarian : ";
 
 	while(true){
 		cout << input;
+		if (key == "anang")
+			cout << "\nok";
+
 		c = getch();
 		if (c == '\n')
 			break;
 		else if ((int) c == 127){
 			p_input = input.length() - 1;
 			input.erase(p_input, 1);
+
+			p_input = key.length() - 1;
+			key.erase(p_input, 1);
 		}
 		else{
 			input += c;
+			key += c;
 		}
+
 		clrscr();
 	}
 

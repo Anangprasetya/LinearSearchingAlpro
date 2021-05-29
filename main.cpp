12c46638e932b2909b532bfe96bf41ecfa50222d
Anang <tyoandini@gmail.com>
2021-05-29 17:32:48 +0700

    membuat dasar live search

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
 #include "myConio.h"
 
-int main(){return 0;}
+int main(){
+	char c;
+	string input = "Input Pencarian : ";
+
+	while(true){
+		cout << input;
+		c = getch();
+		if (c == '\n')
+			break;
+		input += c;
+		clrscr();
+	}
+
+	return 0;
+}
